9aaac936a124254ac2e51b715a87ac0099a1d122
Maks-Chornyi <--replace-all>
2021-06-09 13:16:39 +0300

    task1: endpoints implemented

--- src/main/java/com/makschornyi/rebasetrainingproj/model/user/UserController.java
+++ src/main/java/com/makschornyi/rebasetrainingproj/model/user/UserController.java
@@ -1,12 +1,30 @@
 package com.makschornyi.rebasetrainingproj.model.user;
 
 import com.makschornyi.rebasetrainingproj.configs.API;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping(API.ROOT)
+@RequestMapping(API.ROOT + "/users")
+@RequiredArgsConstructor
 public class UserController {
 
+    private final UserService userService;
 
+    @GetMapping("/users")
+    public List<User> getAllUsers() {
+        return userService.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public User getUserById(@PathVariable String id) {
+        return userService.findById(id);
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
 }
