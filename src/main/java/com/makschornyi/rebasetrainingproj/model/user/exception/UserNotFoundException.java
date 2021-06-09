c449fc7dc35a70d36c75d6839d2427be0de6c276
Maks-Chornyi <--replace-all>
2021-06-09 13:23:52 +0300

    task2: provided new exception

--- src/main/java/com/makschornyi/rebasetrainingproj/model/user/exception/UserNotFoundException.java
+++ src/main/java/com/makschornyi/rebasetrainingproj/model/user/exception/UserNotFoundException.java
@@ -1,4 +1,4 @@
-package com.makschornyi.rebasetrainingproj.model.user;
+package com.makschornyi.rebasetrainingproj.model.user.exception;
 
 public class UserNotFoundException extends RuntimeException{
     public UserNotFoundException(String msg) {
